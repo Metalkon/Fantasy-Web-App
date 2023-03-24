de259f9881748721bd2f817c49a26f1ae738fef2
Metalkon <chrisc@bpgstudios.com>
2023-03-24 08:12:36 -0300

    Add CRUD capabilities to the ItemController

--- Fantasy Web API/Controllers/ItemsController.cs
+++ Fantasy Web API/Controllers/ItemsController.cs
@@ -23,13 +23,88 @@ namespace Fantasy_Web_API.Controllers
 
         // Retrieves a list of all "items" from the database as a JSON response.
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Item>>> GetAllItems()
+        public async Task<ActionResult<IEnumerable<ItemDTO>>> GetAllItems()
         {
-            if (_db.Items == null)
+            var result = await _db.Items.ToListAsync();
+            if (result == null)
             {
                 return NotFound();
             }
-            return await _db.Items.ToListAsync();
+            return Ok(result);
+        }
+
+        // Retrieves a single item by id as a JSON response.
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<IEnumerable<ItemDTO>>> GetItemById(int id)
+        {
+            var result = await _db.Items.FirstOrDefaultAsync(x => x.Id == id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        // Creates a new database entry
+        [HttpPost]
+        public async Task<ActionResult<ItemDTO>> CreateItem(ItemDTO item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var newItem = new Item()
+            {
+                Name = item.Name
+            };
+            _db.Items.Add(newItem);
+            await _db.SaveChangesAsync();
+            var newItemDTO = new ItemDTO()
+            {
+                Id = newItem.Id,
+                Name = newItem.Name
+            };
+            return CreatedAtAction(nameof(GetItemById), new { id = newItemDTO.Id }, newItemDTO);
+        }
+
+        // Deletes a single item entry by id.
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<ActionResult<ItemDTO>> DeleteItemById(int id)
+        {
+            var result = await _db.Items.FirstOrDefaultAsync(x => x.Id == id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            _db.Items.Remove(result);
+            await _db.SaveChangesAsync();
+            return NoContent();
+        }
+
+        // Updates a single item entry by id.
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<ActionResult<ItemDTO>> UpdateItemById(int id, ItemDTO item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var existingItem = await _db.Items.FindAsync(id);
+            if (existingItem == null)
+            {
+                return NotFound();
+            }
+            existingItem.Name = item.Name;
+            await _db.SaveChangesAsync();
+            var updatedItem = new ItemDTO()
+            {
+                Id = existingItem.Id,
+                Name = existingItem.Name
+            };
+            return Ok(updatedItem);
         }
     }
-}
+}   
