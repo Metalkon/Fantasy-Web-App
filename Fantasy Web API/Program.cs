6706f8879319576e02734412c17c271165feb183
Metalkon <chrisc@bpgstudios.com>
2023-05-07 04:14:41 -0300

    auth requirements in program.cs

--- Fantasy Web API/Program.cs
+++ Fantasy Web API/Program.cs
@@ -1,4 +1,5 @@
 using Fantasy_Web_API.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 
 namespace Fantasy_Web_API
@@ -25,6 +26,30 @@ namespace Fantasy_Web_API
             })
             );
 
+            // Jwt/Auth stuff
+            builder.Services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = config["JwtSettings:Issuer"],
+                    ValidAudience = config["JwtSettings:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey
+                    (Encoding.UTF8.GetBytes(config["JwtSettings:Key"]!)),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true
+
+                };
+            });
+
+            builder.Services.AddAuthorization(); // *
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
@@ -41,7 +66,8 @@ namespace Fantasy_Web_API
 
             app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+            app.UseAuthentication();
+            app.UseAuthorization(); // *
 
             app.UseCors();
 
