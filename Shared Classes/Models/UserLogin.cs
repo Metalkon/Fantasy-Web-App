9424c3230e285fd9e6507a1f2e7d5177abef4dd1
Metalkon <chrisc@bpgstudios.com>
2023-07-09 14:29:00 -0300

    Replaced the temporary password login code with a url for user login's

--- Shared Classes/Models/UserLogin.cs
+++ Shared Classes/Models/UserLogin.cs
@@ -4,9 +4,7 @@ namespace Shared_Classes.Models
 {
     public class UserLogin
     {
-        public string Username { get; set; }
         [EmailAddress]
         public string Email { get; set; }
-        public string? LoginCode { get; set; } = string.Empty;
     }
 }
